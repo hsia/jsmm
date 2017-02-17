dbfcb7576607ef0de466e4330a8fee495ef2dd44
sunchao <6724679@qq.com>
2017-02-17 13:44:57 +0800

    index.js add toolbar

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -6,18 +6,31 @@ $(function () {
     var dg = $('#person-lists');
 
     var toolbar = [{
-			text:'添加',
-			iconCls:'icon-add',
-			handler:function(){alert('add')}
-		},'-',{
-			text:'删除',
-			iconCls:'icon-cancel',
-			handler:function(){alert('cut')}
-		},'-',{
-			text:'Save',
-			iconCls:'icon-save',
-			handler:function(){alert('save')}
-		}];
+        text: '添加',
+        iconCls: 'icon-add',
+        handler: function () {
+            $('#member-dialog').dialog({
+                width: 800,
+                height: 600,
+                title:'添加社员',
+                closed: false,
+                cache: false,
+                modal: true
+            });
+        }
+    }, '-', {
+        text: '删除',
+        iconCls: 'icon-cancel',
+        handler: function () {
+            alert('cut')
+        }
+    }, '-', {
+        text: 'Save',
+        iconCls: 'icon-save',
+        handler: function () {
+            alert('save')
+        }
+    }];
 
     dg.datagrid({
         iconCls: 'icon-ok',
@@ -40,39 +53,39 @@ $(function () {
             {field: 'name', title: '姓名', width: 110, align: 'left'},
             {field: 'age', title: '年龄', width: 100, align: 'left'}
         ]],
-        loader:function (param,success) {
-           var defaultUrl = '/members';
-           $.get(defaultUrl, function (data) {
+        loader: function (param, success) {
+            var defaultUrl = '/members';
+            $.get(defaultUrl, function (data) {
                 success(data)
-           });
+            });
         },
-        loadFilter: function(data){
+        loadFilter: function (data) {
             if (typeof data.length == 'number' && typeof data.splice == 'function') {
-            data = {
-                total: data.length,
-                rows: data
+                data = {
+                    total: data.length,
+                    rows: data
+                }
             }
-        }
-        var opts = dg.datagrid('options');
-        var pager = dg.datagrid('getPager');
-        pager.pagination({
-            onSelectPage: function (pageNum, pageSize) {
-                opts.pageNumber = pageNum;
-                opts.pageSize = pageSize;
-                pager.pagination('refresh', {
-                    pageNumber: pageNum,
-                    pageSize: pageSize
-                });
-                dg.datagrid('loadData', data);
+            var opts = dg.datagrid('options');
+            var pager = dg.datagrid('getPager');
+            pager.pagination({
+                onSelectPage: function (pageNum, pageSize) {
+                    opts.pageNumber = pageNum;
+                    opts.pageSize = pageSize;
+                    pager.pagination('refresh', {
+                        pageNumber: pageNum,
+                        pageSize: pageSize
+                    });
+                    dg.datagrid('loadData', data);
+                }
+            });
+            if (!data.originalRows) {
+                data.originalRows = (data.rows);
             }
-        });
-        if (!data.originalRows) {
-            data.originalRows = (data.rows);
-        }
-        var start = (opts.pageNumber - 1) * parseInt(opts.pageSize);
-        var end = start + parseInt(opts.pageSize);
-        data.rows = (data.originalRows.slice(start, end));
-        return data;
+            var start = (opts.pageNumber - 1) * parseInt(opts.pageSize);
+            var end = start + parseInt(opts.pageSize);
+            data.rows = (data.originalRows.slice(start, end));
+            return data;
         },
         onCheck: function (index, row) {
             choiceRows = dg.datagrid("getChecked");
@@ -99,7 +112,6 @@ $(function () {
     function getDataTime(value, row, index) {
         return row.dataTime.substr(0, 8);
     }
-
 
 
 });
