cb2ace2e0f79c3ccb793a19e5ec4aea4bbefea69
wt03201 <wangtao03201@gmail.com>
2017-02-23 10:27:31 +0800

    resume

--- www/scripts/tab/politicsresume.js
+++ www/scripts/tab/politicsresume.js
@@ -67,81 +67,25 @@ $(function () {
             toolbar: toolbar,
             columns: [[
                 {
-                    field: 'eduSchoolName',
-                    title: '学校(单位)名称',
-                    width: 150,
-                    align: 'left',
-                    editor: {type: 'textbox', options: {}}
-                },
-                {
-                    field: 'eduStartingDate',
-                    title: '入学时间',
+                    field: 'resumeDate',
+                    title: '日期',
                     width: 60,
                     align: 'left',
                     editor: {type: 'datebox', options: {}}
                 },
                 {
-                    field: 'eduGraduateDate',
-                    title: '毕业时间',
-                    width: 60,
-                    align: 'left',
-                    editor: {type: 'datebox', options: {}}
-                },
-                {
-                    field: 'eduMajor',
-                    title: '专业',
-                    width: 120,
+                    field: 'resumeEvent',
+                    title: '事件',
+                    width: 160,
                     align: 'left',
                     editor: {type: 'textbox', options: {}}
                 },
                 {
-                    field: 'eduEducation',
-                    title: '学历',
-                    width: 110,
-                    align: 'left',
-                    editor: {
-                        type: 'combobox',
-                        options: {
-                            valueField: 'value',
-                            textField: 'text',
-                            method: 'get',
-                            url: 'data/education.json',
-                            prompt: '请选择'
-                        }
-                    }
-                },
-                {
-                    field: 'eduDegree',
-                    title: '学位',
-                    width: 110,
-                    align: 'left',
-                    editor: {
-                        type: 'combobox',
-                        options: {
-                            valueField: 'value',
-                            textField: 'text',
-                            method: 'get',
-                            url: 'data/degree.json',
-                            prompt: '请选择'
-                        }
-                    }
-                },
-                {
-                    field: 'eduEducationType',
-                    title: '教育类别',
+                    field: 'resumeTime',
+                    title: '时间',
                     width: 80,
                     align: 'left',
-                    editor: {
-                        type: 'combobox',
-                        options: {
-                            valueField: 'value',
-                            textField: 'text',
-                            method: 'get',
-                            url: 'data/educationType.json',
-                            prompt: '请选择',
-                            panelHeight: 'auto'
-                        }
-                    }
+                    editor: {type: 'textbox', options: {}}
                 }
             ]],
             onClickRow: function (index, row) {
