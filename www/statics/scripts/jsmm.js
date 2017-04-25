fed209241022515fa8924e73cd57a97476c42257
wt03201 <wangtao03201@gmail.com>
2017-04-25 15:43:13 +0800

    将index.html中的js文件合成到一个js文件中

--- www/statics/scripts/jsmm.js
+++ www/statics/scripts/jsmm.js
@@ -24727,323 +24727,6 @@ function mySort(index, type, $grid) {
     }
 
 }
-var GridTab = function (tabId, $grid) {
-    this.member = null;
-    this.memberId = null;
-    this.tabId = tabId;
-    this.$grid = $grid;
-    this.editIndex = undefined;
-    this.documentId = null;
-};
-
-GridTab.prototype.endEditing = function () {
-    if (this.editIndex == undefined) {
-        return true;
-    }
-    if (this.$grid.datagrid('validateRow', this.editIndex)) {
-        this.$grid.datagrid('endEdit', this.editIndex);
-        this.editIndex = undefined;
-        return true;
-    } else {
-        return false;
-    }
-};
-
-GridTab.prototype.addRow = function () {
-    if (!this.memberId) {
-        $.messager.alert('提示信息', '请选择一行社员信息!', 'error');
-        return;
-    }
-    if (this.endEditing()) {
-        var row = this.$grid.datagrid('getSelected');
-        if (row != null) {
-            this.editIndex = this.$grid.datagrid('getRowIndex', row);
-            console.log(this.editIndex)
-            this.$grid.datagrid('insertRow', {
-                index: this.editIndex,	// index start with 0
-                row: {}
-            });
-            this.$grid.datagrid('selectRow', this.editIndex).datagrid('beginEdit', this.editIndex);
-        } else {
-            this.$grid.datagrid('appendRow', {});
-            this.editIndex = this.$grid.datagrid('getRows').length - 1;
-            this.$grid.datagrid('selectRow', this.editIndex).datagrid('beginEdit', this.editIndex);
-        }
-    }
-};
-
-//上移记录
-GridTab.prototype.moveUp = function () {
-    if (this.editIndex == undefined) {
-        return
-    }
-
-    this.$grid.datagrid('endEdit', this.editIndex)
-    var row = this.$grid.datagrid('getSelected');
-    var index = this.$grid.datagrid('getRowIndex', row);
-    mySort(index, 'up', this.$grid);
-};
-
-//下移记录
-GridTab.prototype.moveDown = function () {
-    if (this.editIndex == undefined) {
-        return
-    }
-    this.$grid.datagrid('endEdit', this.editIndex)
-    var row = this.$grid.datagrid('getSelected');
-    var index = this.$grid.datagrid('getRowIndex', row);
-    mySort(index, 'down', this.$grid);
-};
-
-
-GridTab.prototype.removeRow = function () {
-    // if (!this.editIndex) {
-    // this.$grid.datagrid('cancelEdit', this.editIndex)
-    //     .datagrid('deleteRow', this.editIndex);
-    // this.editIndex = null;
-    // }
-    if (this.editIndex == undefined || this.editIndex < 0) {
-        return
-    }
-    this.$grid.datagrid('cancelEdit', this.editIndex)
-        .datagrid('deleteRow', this.editIndex);
-
-    rows = this.$grid.datagrid('getRows')
-
-    if (this.editIndex >= rows.length) {
-        this.editIndex = (rows.length - 1);
-        this.$grid.datagrid('selectRow', this.editIndex);
-    } else {
-        this.$grid.datagrid('selectRow', this.editIndex);
-    }
-};
-
-GridTab.prototype.saveRow = function () {
-    var memberInfo = {};
-    var that = this;
-    if (!this.memberId) {
-        return;
-    }
-    $.get('/members/tab/' + this.memberId, function (data) {
-        memberInfo = data;
-        if (that.endEditing()) {
-            memberInfo[that.tabId] = that.$grid.datagrid('getRows');
-            $.ajax({
-                url: '/members/' + memberInfo._id,
-                type: 'PUT',
-                data: JSON.stringify(memberInfo),
-                success: function (data) {
-                    //删除成功以后，重新加载数据，并将choiceRows置为空。
-                    $.messager.alert('提示', '数据保存成功!', 'info');
-                },
-                error: function (data) {
-                    $.messager.alert('提示', '数据更新失败!', 'error');
-                }
-            });
-        }
-    });
-};
-
-GridTab.prototype.docUpload = function () {
-    if (this.memberId == null) {
-        $.messager.alert('提示信息', '请选择一行社员信息!', 'error');
-        return;
-    }
-    console.log(this.memberId);
-    var that = this;
-    $('#doc_upload_form').form('clear');
-    $('#member_doc').dialog({
-        width: 300,
-        height: 200,
-        title: '文档上传',
-        closed: false,
-        cache: false,
-        modal: true,
-        buttons: [{
-            iconCls: 'icon-import',
-            text: '导入',
-            handler: function () {
-                $('#member_doc').dialog('close');
-                $.messager.progress({
-                    title: 'Please waiting',
-                    msg: 'Loading data...'
-                });
-                $('#doc_upload_form').form('submit', {
-                    url: '/document/' + that.memberId + '/' + that.tabId,
-                    success: function (data) {
-                        that.reloadGridRemote();
-                        $.messager.progress('close');
-                        $.messager.alert('提示信息', '文档上传成功！', 'info');
-                    }
-                });
-            }
-        }, {
-            iconCls: 'icon-cancel',
-            text: '取消',
-            handler: function () {
-                $('#doc_upload_form').form('clear');
-                $('#member_doc').dialog('close');
-            }
-        }]
-    })
-};
-
-GridTab.prototype.docDelete = function () {
-    if (this.documentId == null) {
-        $.messager.alert('提示信息', '未选择文档！', 'info');
-        return;
-    }
-    var that = this;
-    $.messager.confirm('删除提示', '确定删除文档?', function (r) {
-        if (r) {
-            $.ajax({
-                url: '/document/' + that.documentId,
-                type: 'DELETE',
-                success: function (data) {
-                    that.reloadGridRemote();
-                    $.messager.alert('提示信息', '删除文档成功！', 'info');
-                },
-                error: function (data) {
-                    $.messager.alert('提示信息', '删除文档失败！', 'error');
-                }
-            });
-        }
-    });
-};
-
-GridTab.prototype.buildGrid = function (toolbar, columns) {
-    var height = $("#member-info").height();
-    var that = this;
-    this.$grid.datagrid({
-        iconCls: 'icon-ok',
-        height: height * 0.89,
-        rownumbers: true,
-        nowrap: true,
-        striped: true,
-        fitColumns: true,
-        loadMsg: '数据装载中......',
-        allowSorts: true,
-        remoteSort: true,
-        multiSort: true,
-        singleSelect: true,
-        toolbar: toolbar,
-        columns: [columns],
-        onClickRow: function (index, row) {
-            if (that.editIndex != index) {
-                if (that.endEditing()) {
-                    that.$grid.datagrid('selectRow', index).datagrid('beginEdit', index);
-                    that.editIndex = index;
-                } else {
-                    that.$grid.datagrid('selectRow', that.editIndex);
-                }
-            }
-        },
-        onBeginEdit: function (index, row) {
-            $(".combo").click(function () {
-                $(this).prev().combobox("showPanel");
-            });
-        }
-    });
-};
-
-GridTab.prototype.buildDocGrid = function (toolbar, columns) {
-    var gridHeight = $("#member-info").height();
-    var that = this;
-    this.$grid.datagrid({
-        iconCls: 'icon-ok',
-        height: gridHeight * 0.89,
-        rownumbers: true,
-        nowrap: true,
-        striped: true,
-        fitColumns: true,
-        loadMsg: '数据装载中......',
-        allowSorts: true,
-        remoteSort: true,
-        multiSort: true,
-        singleSelect: true,
-        toolbar: toolbar,
-        columns: [columns],
-        onSelect: function (rowIndex, rowData) {
-            that.documentId = rowData._id;
-        }
-    });
-};
-
-GridTab.prototype.reloadGrid = function (clear) {
-    if (clear) {
-        this.$grid.datagrid('loadData', []);
-        return;
-    }
-    if (!$.isEmptyObject(this.member)) {
-        if (!$.isEmptyObject(this.member[this.tabId])) {
-            this.$grid.datagrid('loadData', this.member[this.tabId]);
-        } else {
-            this.$grid.datagrid('loadData', []);
-        }
-    }
-};
-
-GridTab.prototype.reloadGridRemote = function () {
-    var that = this;
-    $.get('/members/' + this.memberId, function (data) {
-        if (!$.isEmptyObject(data)) {
-            if (!$.isEmptyObject(data[that.tabId])) {
-                that.$grid.datagrid('loadData', data[that.tabId]);
-            } else {
-                that.$grid.datagrid('loadData', []);
-            }
-        }
-    });
-};
-
-GridTab.prototype.registerListeners = function () {
-    var that = this;
-    window.addEventListener("grid-row-selection", function (event) {
-        that.member = event.detail;
-        that.memberId = that.member._id;
-        that.reloadGrid();
-    });
-    window.addEventListener("grid-dg-refresh", function (event) {
-        that.member = null;
-        that.memberId = null;
-        that.reloadGrid();
-    });
-    window.addEventListener("grid-row-deleteRow", function (event) {
-        if (event.detail.success == true) {
-            that.reloadGrid(true);
-        }
-    });
-    window.addEventListener("tree-row-selection", function (event) {
-        that.reloadGrid(true);
-    });
-};
-
-function mySort(index, type, $grid) {
-    if ("up" == type) {
-        if (index != 0) {
-            var toup = $grid.datagrid('getData').rows[index];
-            var todown = $grid.datagrid('getData').rows[index - 1];
-            $grid.datagrid('getData').rows[index] = todown;
-            $grid.datagrid('getData').rows[index - 1] = toup;
-            $grid.datagrid('refreshRow', index);
-            $grid.datagrid('refreshRow', index - 1);
-            $grid.datagrid('selectRow', index - 1);
-        }
-    } else if ("down" == type) {
-        var rows = $grid.datagrid('getRows').length;
-        if (index != rows - 1) {
-            var todown = $grid.datagrid('getData').rows[index];
-            var toup = $grid.datagrid('getData').rows[index + 1];
-            $grid.datagrid('getData').rows[index + 1] = todown;
-            $grid.datagrid('getData').rows[index] = toup;
-            $grid.datagrid('refreshRow', index);
-            $grid.datagrid('refreshRow', index + 1);
-            $grid.datagrid('selectRow', index + 1);
-        }
-        $grid
-    }
-
-}
 /**
  * Created by S on 2017/2/16.
  */
@@ -26291,6 +25974,199 @@ $(function () {
 
 })
 ;
+$(function () {
+
+    var gridHeight = $('#membersinfo').height();
+    var $dataGrid = $('#document-list');
+
+    var toolbar = [{
+        text: '高级查询',
+        iconCls: 'icon-search',
+        handler: function () {
+            documentsSearch();
+        }
+    }];
+
+    $dataGrid.datagrid({
+        iconCls: 'icon-ok',
+        height: gridHeight,
+        rownumbers: true,
+        pageSize: 20,
+        nowrap: true,
+        striped: true,
+        fitColumns: true,
+        loadMsg: '数据装载中......',
+        pagination: true,
+        allowSorts: true,
+        multiSort: false,
+        singleSelect: true,
+        remoteSort: true,
+        toolbar: toolbar,
+        url: '/documents1',
+        method: 'GET',
+        columns: [[
+            {field: 'fileName', title: '文件名称', width: 110, sortable: false, align: 'left'},
+            {
+                field: 'docType',
+                title: '文件类型',
+                width: 60,
+                sortable: false,
+                align: 'left',
+                formatter: changeType
+            },
+            {field: 'name', title: '社员姓名', width: 60, sortable: false, align: 'left'},
+            {field: 'branch', title: '所属支社', width: 90, sortable: false, align: 'left'},
+            {field: 'fileUploadTime', title: '创建时间', width: 80, sortable: true, align: 'left'},
+            {
+                field: 'clickDownload',
+                title: '操作',
+                width: 60,
+                sortable: false,
+                align: 'left',
+                formatter: function (value, row, index) {
+                    var path = "/document/" + row._id + "/" + row.fileName;
+                    return '<a href= ' + path + '>下载</a>';
+                }
+            }
+        ]]
+    });
+
+    //点击组织机构触发事件
+    var organName = null;
+    window.addEventListener("tree-row-selection", function (event) {
+        organName = event.detail;
+        if (organName != null) {
+            var getCurrentRows = $dataGrid.datagrid('options').pageSize;
+            $dataGrid.datagrid('load', {
+                'page': 1,
+                'rows': getCurrentRows,
+                'branch': organName
+            })
+        } else {
+            return false;
+        }
+    });
+
+    window.addEventListener("organ-tree-operation", function (event) {
+        var getCurrentRows = $dataGrid.datagrid('options').pageSize;
+        $dataGrid.datagrid('load', {
+            'page': 1,
+            'rows': getCurrentRows
+        })
+    });
+
+    $('#tabsAll').tabs({
+        border: false,
+        onSelect: function (title, index) {
+            $("#tb-form").form('clear');
+            var node = $('#organTree').tree('getSelected');
+            console.log(title + "," + index);
+            if (index == 1) {
+                var param = {'page': 1, 'rows': getCurrentRows};
+                if (node != null) {
+                    param.branch = node.id;
+                }
+                var getCurrentRows = $dataGrid.datagrid('options').pageSize;
+                $dataGrid.datagrid('load', param);
+            }
+        }
+    });
+
+    function changeType(value, row, index) {
+        var result = '';
+        switch (value) {
+            case 'researchReport':
+                result = '调研报告';
+                break;
+            case 'politicsInfo':
+                result = '参政议政信息';
+                break;
+            case 'unitedTheory':
+                result = '统战理论';
+                break;
+            case 'propaganda':
+                result = '宣传稿';
+                break;
+        }
+
+        return result;
+    }
+
+    $("#document-search-form").submit(function (event) {
+        var formData = $(this).serializeArray();
+        var documentInfo = {};
+        $.each(formData, function (index, element) {
+            documentInfo[element.name] = element.value;
+        });
+        documentInfo.branch = (branch == null ? '' : branch);
+        if (documentInfo.startDate != '' && documentInfo.endDate == '') {
+            var mydate = new Date();
+            currYear = mydate.getFullYear();
+            currMonth = mydate.getMonth() + 1;
+            currDay = mydate.getDate();
+            documentInfo.endDate = currYear + "-" + ((currMonth < 10) ? ("0" + currMonth) : currMonth) + "-" + ((currDay < 10) ? ("0" + currDay) : currDay);
+        } else if (documentInfo.startDate == '' && documentInfo.endDate != '') {
+            documentInfo.startDate = '1970-01-01';
+        } else {
+
+        }
+        $('#document-search').dialog('close');
+        $('#document-search-form').form('clear');
+
+        var getCurrentRows = $dataGrid.datagrid('options').pageSize;
+        var param = {
+            'page': 1,
+            'rows': getCurrentRows,
+            'documentInfo': JSON.stringify(documentInfo),
+            'branch': (organName == null ? '' : organName)
+        };
+        $dataGrid.datagrid('load', param);
+
+        // $dataGrid.datagrid({
+        //     loader: function (param, success) {
+        //         param.documentInfo = documentInfo;
+        //         param.branch = (organName == null ? '' : organName);
+        //         var defaultUrl = '/documents';
+        //         $.post(defaultUrl, JSON.stringify(param), function (data) {
+        //             success(data)
+        //         }, 'json');
+        //     }
+        // })
+    });
+
+    function documentsSearch() {
+
+        $('#docTypeD').combobox({
+            valueField: 'value',
+            textField: 'text',
+            url: 'static/data/documenttype.json',
+            method: 'get'
+        })
+        $('#document-search').dialog({
+            width: 600,
+            height: 300,
+            title: '社员查询',
+            closed: false,
+            cache: false,
+            modal: true,
+            buttons: [{
+                iconCls: 'icon-ok',
+                text: '查询',
+                handler: function () {
+                    $('#document-search-form').trigger('submit');
+                }
+            }, {
+                iconCls: 'icon-cancel',
+                text: '取消',
+                handler: function () {
+                    $('#document-search-form').form('clear');
+                    $('#document-search').dialog('close');
+                }
+            }]
+        });
+        $('#documentSearchKeyWord').next('span').find('input').focus();
+    }
+});
 /**
  * Created by S on 2017/2/21.
  */
@@ -28112,68 +27988,65 @@ $(function () {
  * Created by S on 2017/2/22.
  */
 
-//专业技术
+// 专家情况
 $(function () {
 
-    var $grid = $("#professional-list");
-    var tabId = 'professionalSkill';
+    var $grid = $("#professor-list");
+    var tabId = 'professor';
     var gridTab = new GridTab(tabId, $grid);
 
     var columns = [
         {
-            field: 'proProjectName',
-            title: '项目名称',
-            width: 120,
-            align: 'left',
-            editor: {type: 'textbox', options: {}}
-        },
-        {
-            field: 'proProjectType',
-            title: '项目类型',
-            width: 90,
-            align: 'left',
-            editor: {
-                type: 'combobox',
-                options: {
-                    valueField: 'value',
-                    textField: 'text',
-                    method: 'get',
-                    url: 'static/data/projectType.json',
-                    prompt: '请选择'
-                }
-            }
-        },
-        {
-            field: 'proProjectCompany',
-            title: '项目下达单位',
-            width: 120,
-            align: 'left',
-            editor: {type: 'textbox', options: {}}
-        },
-        {
-            field: 'proRolesInProject',
-            title: '项目中所任角色',
+            field: 'professorName',
+            title: '专家名称',
             width: 60,
             align: 'left',
             editor: {
-                type: 'combobox',
-                options: {
-                    valueField: 'value',
-                    textField: 'text',
-                    method: 'get',
-                    url: 'static/data/roleInProject.json',
-                    prompt: '请选择'
-                }
+                type: 'textbox',
+                options: {}
             }
         },
         {
-            field: 'proStartDate',
-            title: '开始时间',
+            field: 'approvalDate',
+            title: '批准时间',
             width: 60,
             align: 'left',
             editor: {type: 'datebox', options: {}}
         },
-        {field: 'porEndDate', title: '结束时间', width: 60, align: 'left', editor: {type: 'datebox', options: {}}},
+        {
+            field: 'approvalCompanyLevel',
+            title: '批准单位级别',
+            width: 80,
+            align: 'left',
+            editor: {type: 'textbox', options: {}}
+        },
+        {
+            field: 'approvalCompanyName',
+            title: '批准单位名称',
+            width: 100,
+            align: 'left',
+            editor: {type: 'textbox', options: {}}
+        },
+        {
+            field: 'govSubsidiesType',
+            title: '政府津贴类别',
+            width: 80,
+            align: 'left',
+            editor: {
+                type: 'textbox',
+                options: {}
+            }
+        },
+        {
+            field: 'subsidiesDate',
+            title: '享受津贴时间',
+            width: 60,
+            align: 'left',
+            editor: {
+                type: 'datebox',
+                options: {}
+            }
+        }
     ];
     var toolbar = [
         {
